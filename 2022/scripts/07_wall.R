582865a15cd7448105fc47cb522ae4ecba61066c
Nicola Rennie <nrennie35@gmail.com>
2022-01-08 15:40:56 +0000

    add prompt 8

--- 2022/scripts/07_wall.R
+++ 2022/scripts/07_wall.R
@@ -6,25 +6,20 @@ set.seed(712022)
 
 #background
 d1 <- data.frame(x=1:36, y=rep(100,36), colour=sample(carto_pal(12, "Bold"), size=36, replace = T))
-p1 <- ggplot() +
-  geom_col(data=d1, mapping=aes(x=x, y=y, fill=I(colour)), colour=NA, width = 1) +
-  coord_cartesian(expand = F) +
-  theme_void()
-p1
 
-#foreground
-x1 <- seq(1,36, by=0.1)
+x1 <- seq(1,36, by=0.01)
 y1 <- -0.25*x1+1+4*sin(0.1*pi*x1)
 d2 <- data.frame(x=rep(x1, 8),
                  y=rep(y1, 8) + rep(0:7, each=length(x1)),
                  colour=rep(sample(carto_pal(12, "Bold"), size=8, replace = F), each=length(x1)))
-p2 <- ggplot() +
+p <- ggplot() +
+  geom_col(data=d1, mapping=aes(x=x, y=y, fill=I(colour)), colour=NA, width = 1) +
   geom_point(data=d2, mapping=aes(x=x, y=y, colour=I(colour), group=colour), size=8) +
   coord_cartesian(expand = F) +
   theme_void() +
   theme(panel.background = element_rect(fill="transparent", colour="transparent"),
         plot.background = element_rect(fill="transparent", colour="transparent"))
-p2
+p
 
 #join
 p <- p1 +
---
+++ 2022/scripts/08_curve.R
@@ -0,0 +1,3 @@
+library(aRt)
+waves(a=10, b=21, main_col="black", bg_col="grey90", s=2021)
+
