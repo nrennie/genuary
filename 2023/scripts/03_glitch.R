19795e69d26639e53a6c3069ffbcb49dfab288f7
nrennie <nrennie35@gmail.com>
2022-12-26 16:33:11 +0000

    add day 3

---
+++ 2023/scripts/03_glitch.R
@@ -0,0 +1,12 @@
+aRt::squares(n = 7,
+             line_col = "#B8DBD9",
+             pattern_col = "#B8DBD9",
+             pattern_fill = "#586F7C",
+             pattern_size = 0.4,
+             size = 1.5,
+             s = 1234)
+
+ggplot2::ggsave(filename = "2023/art/03_glitch.png",
+                width = 600,
+                height = 600,
+                unit = "px")
