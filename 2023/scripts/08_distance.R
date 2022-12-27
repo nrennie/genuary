58dc0755ddd0e2ea26d396e264ba676d9deb63fb
nrennie <nrennie35@gmail.com>
2022-12-27 17:54:49 +0000

    add day 8

---
+++ 2023/scripts/08_distance.R
@@ -0,0 +1,7 @@
+aRt::flow_fields(n = 5000, granualarity = 1000, x_freq = 10,
+                 y_freq = 1, alpha = 0.2, line_col = "#333533",
+                 bg_col = "#E8EDDF", s = 2023)
+ggplot2::ggsave(filename = "2023/art/08_distance.png",
+                width = 600,
+                height = 600,
+                unit = "px")
