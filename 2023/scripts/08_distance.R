1ac27063d6a84b7bdecbd805ffdfb5efea895dd8
nrennie <nrennie35@gmail.com>
2022-12-28 21:42:31 +0000

    add day 8

--- 2023/scripts/08_distance.R
+++ 2023/scripts/08_distance.R
@@ -1,7 +1,21 @@
-aRt::flow_fields(n = 5000, granualarity = 1000, x_freq = 10,
-                 y_freq = 1, alpha = 0.2, line_col = "#333533",
-                 bg_col = "#E8EDDF", s = 2023)
+library(ggplot2)
+library(rayshader)
+library(dplyr)
+
+plot_data <- expand.grid(x = seq(-0.5, 0.5, by = 0.01),
+                         y = seq(-0.5, 0.5, by = 0.01))
+plot_data <- plot_data |>
+  mutate(dists = 1 - sqrt(x^2 + y^2))
+
+ggplot(plot_data,
+       aes(x, y, fill = dists)) +
+  geom_raster() +
+  coord_fixed(expand = FALSE) +
+  PrettyCols::scale_fill_pretty_c("PurpleTangerines", direction = -1) +
+  theme_void() +
+  theme(legend.position = "none")
 ggplot2::ggsave(filename = "2023/art/08_distance.png",
                 width = 600,
                 height = 600,
                 unit = "px")
+
