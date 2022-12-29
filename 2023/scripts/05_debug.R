56e0860d30e0929c9808ef2ec7d3f5e1dbe45bd6
nrennie <nrennie35@gmail.com>
2022-12-29 15:29:44 +0000

    fix spelling

--- 2023/scripts/05_debug.R
+++ 2023/scripts/05_debug.R
@@ -1,4 +1,4 @@
-aRt::flow_fields(n = 5000, granualarity = 1000, x_freq = 10,
+aRt::flow_fields(n = 5000, granularity = 1000, x_freq = 10,
                  y_freq = 1, alpha = 0.2, line_col = "#333533",
                  bg_col = "#E8EDDF", s = 2023)
 ggplot2::ggsave(filename = "2023/art/05_debug.png",
