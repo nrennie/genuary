7d667acd76b9d7f0f46719f2d6e05a23f6461786
nrennie <nrennie35@gmail.com>
2023-01-05 20:22:22 +0000

    add day 24

---
+++ 2023/scripts/24_textile.R
@@ -0,0 +1,7 @@
+aRt::flow_fields(n = 5000, granularity = 1000, x_freq = 1,
+                 y_freq = 15, alpha = 0.15, line_col = "#E6E8E6",
+                 bg_col = "#4C1A57", s = 2023)
+ggplot2::ggsave(filename = "2023/art/24_textile.png",
+                width = 600,
+                height = 600,
+                unit = "px")
