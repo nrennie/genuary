f5008681429501e35c1da133222b50ff173f228b
nrennie <nrennie35@gmail.com>
2023-01-14 13:53:57 +0000

    update day 17

--- 2023/scripts/17_grid.R
+++ 2023/scripts/17_grid.R
@@ -1,9 +1,3 @@
-aRt::puzzles(n = 100,
-             num_groups = 70,
-             col_palette = PrettyCols::prettycols("Beach"),
-             bg_col = "black",
-             s = 1234)
-ggplot2::ggsave(filename = "2023/art/17_grid.png",
-                width = 600,
-                height = 600,
-                unit = "px")
+png("2023/art/17_grid.png", width = 800, height = 800, units = "px", res = 300)
+aRt::windows(n_x = 12, n_y = 12, col_palette = gray.colors(20), linewidth = 2)
+dev.off()
