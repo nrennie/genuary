65c4998c1686e03823a417fd0517738841ea90c4
nrennie <nrennie35@gmail.com>
2025-01-02 19:00:21 +0000

    add day 6

---
+++ 2025/scripts/06_landscape.R
@@ -0,0 +1,7 @@
+fpath <- system.file("images", "tree.jpg", package = "pixR")
+pixR::recolour(
+  img_path = fpath,
+  palette = PrettyCols::prettycols("Teals", direction = -1),
+  rescale = 100,
+  filename = "2025/art/06_landscape.jpg"
+)
