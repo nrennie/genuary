559b16f914acbcd4b3cdd1408c7208a1aef4cfca
nrennie <nrennie35@gmail.com>
2025-01-02 19:32:46 +0000

    add day 9

---
+++ 2025/scripts/09_textile.R
@@ -0,0 +1,8 @@
+aRt::polygons(
+  colours = PrettyCols::prettycols("Disco"),
+  bg_col = "black",
+  rand = TRUE,
+  deg_jitter = 0.4,
+  s = 2025
+)
+ggplot2::ggsave("2025/art/09_textile.png", height = 4, width = 4)
