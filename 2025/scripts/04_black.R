bf402317e3f3a83395d5dfd908e3630688fd7219
nrennie <nrennie35@gmail.com>
2025-01-02 20:12:04 +0000

    add day 4

---
+++ 2025/scripts/04_black.R
@@ -0,0 +1,7 @@
+aRt::disarray(
+  n_x = 20,
+  n_y = 20,
+  col_palette = c("grey5", "grey10", "grey15", "grey20"),
+  grid_col = "black"
+)
+ggplot2::ggsave("2025/art/04_black.png", height = 4, width = 4)
