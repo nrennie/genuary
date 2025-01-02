581bd1884eed8f74ccce77dd5db0dc3c439fc0d1
nrennie <nrennie35@gmail.com>
2025-01-02 19:37:03 +0000

    add day 12

---
+++ 2025/scripts/12_subdivision.R
@@ -0,0 +1,15 @@
+png(
+  filename = "2025/art/12_subdivision.png",
+  res = 300,
+  width = 4,
+  height = 4,
+  unit = "in"
+)
+aRt::divide(
+  num_lines = 25,
+  col_palette = PrettyCols::prettycols("Coast"),
+  rayshade = TRUE,
+  s = 123
+)
+dev.off()
+
