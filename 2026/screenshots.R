d4d80560e01de29174921c876379eb8fed9d570f
Nicola Rennie <42338476+nrennie@users.noreply.github.com>
2026-01-01 14:34:47 +0000

    set up 2026

---
+++ 2026/screenshots.R
@@ -0,0 +1,10 @@
+webshot2::webshot(
+    url = "2026/day-01/index.html",
+    file = "2026/day-01/day-01.png",
+    selector = "#art",
+    expand = c(8, 8, 8, 8),
+    quiet = TRUE,
+    zoom = 2,
+    vwidth = 1000,
+    vheight = 1000
+)
